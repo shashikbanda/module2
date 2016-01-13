cf86115a828fd6b86baf2d5486d5ce09f4c1db5d
LUIS ROCHA <luisrocha1993@gmail.com>
2016-01-13 15:46:14 -0800

    database fixed on my end -Luis

--- routes/private.js
+++ routes/private.js
@@ -10,8 +10,6 @@ router.get('/:userID', function(req, res, next){
 		return userObj
 	}).then(function(userObj){
 		knex('photos').where({userID:id}).then(function(photoObj){
-			// console.log(photoObj)
-			// res.send(photoObj)
 			res.render('privateProfile', {main_picture:userObj[0]['main_picture'], first_name:userObj[0]['first_name'], last_name:userObj[0]['last_name'], role:userObj[0]['role'], userID:userObj[0]['userID'], photoArray:photoObj})
 		})
 	})
@@ -25,12 +23,12 @@ router.post('/:userID', function(req, res, next){
 	var userCity = "San Francisco";
 	var role = req.body.role;
 
-	// knex('users').whereNot({userID: facebookID}).insert({userID:facebookID,
-	// 	first_name: first_name, last_name: last_name, email: userEmail, 
-	// 		city: userCity, main_picture:profPicLink, role: role})
-	// 	.then(function(){
-	// 		res.redirect("/private/"+facebookID);
-	// })
-	res.redirect("/private/"+facebookID)
+	knex('users').whereNot({userID: facebookID}).insert({userID:facebookID,
+		first_name: first_name, last_name: last_name, email: userEmail, 
+			city: userCity, main_picture:profPicLink, role: role})
+		.then(function(){
+			res.redirect("/private/"+facebookID);
+	})
+	// res.redirect("/private/"+facebookID)
 })
 module.exports = router;
